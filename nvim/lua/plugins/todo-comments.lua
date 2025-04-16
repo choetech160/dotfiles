18929b9a638f7ef373a93b44843f5a88f32a25c8
jp <jp@bluefalcon.cf>
2025-04-16 14:42:35 -0400

    Corrected lsp, comments etc

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,5 +1,29 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+<<<<<<< Updated upstream
   opts = {},
+=======
+  keys = {
+
+    { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Search TODOs (Telescope)" },
+    { "<leader>xT", "<cmd>TodoTrouble<cr>", desc = "TODOs (Trouble Panel)" },
+  },
+  opts = {
+    keywords = {
+
+      NICE = {
+        icon = "🔆",
+        color = "#8BC34A", -- Darker green color
+        alt = { "FEATURE", "SUGGESTION" },
+      },
+      REPEAT = { icon = "🌀", color = "#C3B1E1", alt = { "REFACTOR", "DUPLICATE", "REDUNDANT" } },
+      IDEAS = {
+        icon = "💡",
+        color = "#FFD700",
+        alt = { "SOLUTION", "CONCEPT", "POSSIBLE_SOLUTION" },
+      },
+    },
+  },
+>>>>>>> Stashed changes
 }
