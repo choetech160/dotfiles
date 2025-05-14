81fadf8b375acf10a3c23431b6ee924d38bb5a0a
jp <jp@bluefalcon.cf>
2025-05-14 07:40:23 -0400

    Added configuration and keybinding for augment

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.g.augment_workspace_folders = { "~/Projects/SCFJS_dev/" }
--- nvim/lua/plugins/augment.lua
+++ nvim/lua/plugins/augment.lua
@@ -1,4 +1,12 @@
 return {
   "augmentcode/augment.vim",
   event = "VeryLazy",
+
+  keys = {
+
+    { "<leader>ac", "<cmd>Augment chat<cr>", desc = "Send a chat to augment " },
+    { "<leader>an", "<cmd>Augment chat-new<cr>", desc = "Start a new conversation" },
+    { "<leader>at", "<cmd>Augment chat-toggle<cr>", desc = "Toggle conversation" },
+    { "<leader>as", "<cmd>Augment status<cr>", desc = "Get augment status" },
+  },
 }
